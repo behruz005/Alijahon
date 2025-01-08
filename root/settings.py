0304af871bac74461e3890382baa14966f120cb9
behruz005 <ibehruz005@gmail.com>
2025-01-08 14:04:46 +0500

    first commit

--- root/settings.py
+++ root/settings.py
@@ -89,8 +89,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATIC_ROOT = join(BASE_DIR , 'static')
+STATIC_ROOT = join(BASE_DIR , 'static/')
 MEDIA_URL = 'media/'
-MEDIA_ROOT = join(BASE_DIR , 'media')
+MEDIA_ROOT = join(BASE_DIR , 'media/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
